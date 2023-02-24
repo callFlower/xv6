234be28ea77fcbc369dbcff7769174c7aa31948a
callFlower <107831013+callFlower@users.noreply.github.com>
2023-02-24 21:18:13 +1100

    lab1-1 sleep-2

--- user/sleep.c
+++ user/sleep.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     // sleep->argv[0]  100(the number)->argv[1]  ==> argc is 2
     if(argc!=2){
         // fprintf is a C function to print a formatted string to a file. 2->error
+        // fp = fopen ("file.txt", "w+");
+        // fprintf(fp, "%s %s %s %d", "We", "are", "in", 2014);
         fprintf(2,"Usage: sleep <time>\n");
         exit(1);
     }
