fa5b1f60408673dcff82038eb624e371fdcb014e
callFlower <107831013+callFlower@users.noreply.github.com>
2023-02-27 19:34:09 +1100

    lab1-1 find.c--test fmtname

--- user/find.c
+++ user/find.c
@@ -4,6 +4,8 @@
 #include "../kernel/fs.h"
 
 /* In ls.c file, . means current path */
+
+//
 char*
 fmtname(char *path)
 {
@@ -26,25 +28,31 @@ fmtname(char *path)
 void
 ls(char *path)
 {
+    // construct pipe
     char buf[512], *p;
     int fd;
     struct dirent de;
     struct stat st;
 
+    // try to open the file path, if it cannot open, then  print error
     if((fd = open(path, 0)) < 0){
         fprintf(2, "ls: cannot open %s\n", path);
         return;
     }
-
+    // don't know what fstate is, but when we open it correctly, we put/load the information from fd to &st(state)
     if(fstat(fd, &st) < 0){
         fprintf(2, "ls: cannot stat %s\n", path);
         close(fd);
         return;
     }
-
+    // make a judgement to st, whether it's a file or a folder
     switch(st.type){
+        // only file
         case T_FILE:
-            printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+            // print directly
+            // printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+            // test fmtname function, see what have changed
+            printf("path: %s, fmtname(path):%s %d %d %l\n", path,fmtname(path), st.type, st.ino, st.size);
             break;
 
         case T_DIR:
@@ -64,22 +72,24 @@ ls(char *path)
                     printf("ls: cannot stat %s\n", buf);
                     continue;
                 }
-                printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+                // printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+                printf("buf: %s, fmtname(buf):%s %d %d %l\n", buf,path,fmtname(buf), st.type, st.ino, st.size);
             }
             break;
     }
     close(fd);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int i;
-
+    // receiving the parameter. eg: ls a: ls-first, a-second
+    // if only ls, then ls .
     if(argc < 2){
         ls(".");
         exit(0);
     }
+    // if is not, looping read parameter and execute. eg: ls a b
     for(i=1; i<argc; i++)
         ls(argv[i]);
     exit(0);
