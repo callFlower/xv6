79aaba21d1696ea909dc053f2e6b388aa13f3f35
callFlower <107831013+callFlower@users.noreply.github.com>
2023-03-03 12:59:07 +1100

    lab1-1 xargs.c-final

--- user/xargs.c
+++ user/xargs.c
@@ -15,11 +15,31 @@ int main(int argc, char* argv[]){
     printf("what we got in standard input: %s\n", buf);
 
     // second step--get the xargs parameter
-    for (int i = 0; i <argc; ++i) {
-        printf("xargs parameter is argv[%d]: %s\n",i,argv[i]);
+    char* giaoargv[MSGSIZE];
+    int giaoargc = 0;
+    // put the argv parameters first
+    for (int i = 0; i < argc; ++i) {
+        giaoargv[giaoargc] = argv[i];
+        giaoargc++;
     }
-
-
+    // put the previous command parameters second
+    char* p = buf;
+    for (int i = 0; i < MSGSIZE; ++i) {
+        // if encounter \n we need to fork it to a new process and execute both of parent and child
+        if(buf[i] == '\n'){
+            int pid = fork();
+            if(pid==0){
+                buf[i]=0;
+                giaoargv[giaoargc++] = p;
+                giaoargv[giaoargc++] = 0;
+                exec(giaoargv[0],giaoargv);
+                exit(0);
+            }else{
+                p = &buf[i+1];
+            }
+        }
+    }
+    // third step--use exec to execute the command
     exit(0);
 }
 
